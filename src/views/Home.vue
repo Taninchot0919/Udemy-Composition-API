e51341deba7919cfe03f65e0855dd157dbf37bf3
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-04-13 22:50:51 +0700

    [UPDATE] using watch and watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,12 @@
     <input type="text" v-model="search" />
     <p>search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">Stop Watching</button>
   </div>
 </template>
 
-
-
 <script>
-import { computed, ref } from "@vue/runtime-core";
+import { computed, ref, watch, watchEffect } from "@vue/runtime-core";
 export default {
   name: "Home",
   setup() {
@@ -24,12 +23,32 @@ export default {
       "koopa",
       "peach"
     ]);
+
+    // เหมือนเป็นคำสั่งจับตาดู
+    // เช่น ทุกๆครั้งที่ search มีการเปลี่ยนแปลงให้มาทำ function ข้างใน
+    const stopWatch = watch(search, () => {
+      console.log("Watch Function run");
+    });
+
+    //** ถ้าอยากใช้ค่าข้างในใช้ watchEffect แต่ถ้าไม่อนากใช้ควรใช้ watch เฉยๆ **/
+
+    // ทำงานแบบจับตาดูเหมือนกันแต่เป็นการจับตาดูค่าข้างในด้วยว่ามีอะไร
+    const stopWatchEffect = watchEffect(() => {
+      console.log("watchEffect Function run", search.value);
+    });
+
     const matchingNames = computed(() => {
       // name ข้างใน filter จะหมายถึง name แต่ละตัว เหมือนๆกับ name in names
       return names.value.filter(name => name.includes(search.value));
     });
 
-    return { names, search, matchingNames };
+    // สามารถหยุด watch และ watchEffect ได้โดยการเหมือนเรียกใช้ซ้ำ
+    const handleClick = ()=> {
+      stopWatch()
+      stopWatchEffect()
+    }
+
+    return { names, search, matchingNames, handleClick};
   }
 };
 </script>
